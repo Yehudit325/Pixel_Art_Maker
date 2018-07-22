0ccd377ab6ff6c7633316be9633b8ec243cfbc47
Yehudit Shay <yehudithalb@gmail.com>
2018-07-22 16:30:27 +0300

    adding table

--- designs.js
+++ designs.js
@@ -6,6 +6,15 @@ function makeGrid(event) {
   height = document.getElementById("gridHeight").value;
   width = document.getElementById("gridWidth").value;
   console.log("The form was submitted");
+
+  const table = document.getElementById("pixelCanvas");
+
+  for (let i = 0; i < height; i++) {
+    let row = table.insertRow(0);
+    for (let j = 0; j < width; j++) {
+      row.insertCell(0);
+    }
+  }
 }
 
 document.getElementById("gridSize").addEventListener("submit", makeGrid);
